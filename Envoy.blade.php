a03f369ecf48b3105f5ef3b6f711d2009f5d878b
Nelson Ameyo <nelson@blackpay.co.ke>
2018-01-27 23:23:12 +0300

    This is an automated deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,7 +4,6 @@
 @story('deploy')
 push-to-git
 pull-to-live
-build-complete
 @endstory
 
 {{--Push local changes to remote Git repo--}}
@@ -17,44 +16,8 @@ git push -u origin master
 
 {{--Pull new changes from remote repo to VPS --}}
 @task('pull-to-live', ['on'=>'production'])
-cd /var/www/html/growthpad
+cd /var/www/help.lipasafe.com
 git fetch --all
 git reset --hard origin/master
-composer update
-@endtask
-
-
-
-
-@task('rebuild-production', ['on'=>'production'])
-cd /var/www/html/growthpad
-{{--  composer install  --}}
-
-{{-- Rebuild application --}}
-php artisan migrate:refresh --seed
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-
-{{-- Fix permissions --}}
-chmod -R 0777 storage
-
-{{-- Rebuild autoload --}}
-composer dump-autoload
-
-{{-- Truncate app log --}}
-rm storage/logs/laravel.log
-touch storage/logs/laravel.log
-chmod 0777 storage/logs/laravel.log
-
-{{-- Restart queue workers --}}
-php artisan queue:restart
-
-{{--  cd public  --}}
-{{--  npm install  --}}
-
-@endtask
-
-@task('build-complete', ['on'=>'local'])
-notify-send 'Devops Complete' 'The staging server has been successfully updated'
+mkdocs build --clean
 @endtask
