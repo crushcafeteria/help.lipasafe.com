7435224f4032408c6a11b352a61057ee29de0b43
Nelson Ameyo <nelson@lipasafe.com>
2018-07-04 18:15:44 +0300

    This is an automated deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['local'=>'127.0.0.1', 'production'=>'root@dock.lipasafe.com'])
+@servers(['local'=>'127.0.0.1', 'production'=>'root@sodium.co.ke'])
 
 {{--Deploy application--}}
 @story('deploy')
